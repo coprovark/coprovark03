82f1a56dccb9dfad15fb471bf5247bcaee4954ef
thipthida <thipthida.tcs59@ubru.ac.th>
2018-03-07 18:42:02 +0700

    profile

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('hello', function () {
-    return view('hello');
+Route::get('up', function () {
+    return view('Profile');
 });
 
